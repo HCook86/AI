f5a6b9158f2ff7f2e85ba257bd6c6a6cc11d3ea0
HCook86 <93165051+HCook86@users.noreply.github.com>
2023-01-21 21:38:37 +0100

    Update visualize.py

--- visualize.py
+++ visualize.py
@@ -9,8 +9,16 @@
 
 from manim import *
 
+class Intro(Scene):
+    def construct(self):
+        text = Text("3Blue1Brown")
+        logo = ImageMobject("./3b1b.png")
+        logo.scale(0.35)
 
+        self.play(text.animate.shift(LEFT*2.5), logo.animate.shift(RIGHT*2.5))
+        self.wait(5)
 
+# Explicando la estructura de una red neuronal
 class Estructura(Scene):
     def construct(self):
         RADIUS = 0.3
@@ -26,17 +34,11 @@ class Estructura(Scene):
 
         # Input nodes
         node1 = Circle(RADIUS, color=CIRCLE_COLOR).move_to([LAYER_1_X,3,0])  # create a circle  # set the color and transparency
-
         node2 = Circle(RADIUS, color=CIRCLE_COLOR).move_to([LAYER_1_X,2,0])
-
         node3 = Circle(RADIUS, color=CIRCLE_COLOR).move_to([LAYER_1_X,1,0])
-
         node4 = Circle(RADIUS, color=CIRCLE_COLOR).move_to([LAYER_1_X,0,0])
-
         node5 = Circle(RADIUS, color=CIRCLE_COLOR).move_to([LAYER_1_X,-1,0])
-
         node6 = Circle(RADIUS, color=CIRCLE_COLOR).move_to([LAYER_1_X,-2,0])
-
         node7 = Circle(RADIUS, color=CIRCLE_COLOR).move_to([LAYER_1_X,-3,0])
         
 
@@ -111,6 +113,7 @@ class Estructura(Scene):
         self.wait(1)
 
 
+# Explicando el cálculo de una inteligencia artificial
 class Formula(Scene):
     def construct(self):
         RADIUS = 0.5
@@ -122,17 +125,17 @@ class Formula(Scene):
 
         # Input nodes
         node1 = DrawBorderThenFill(Circle(RADIUS, color=CIRCLE_COLOR).move_to([LAYER_1_X,2,0]))  # create a circle  # set the color and transparency
-        text1 = Create(Text("a1", font_size=20, color=RED).move_to([LAYER_1_X,2,0]))
+        text1 = Create(MathTex(r"a_{1}", font_size=35, color=RED).move_to([LAYER_1_X,2,0]).set_color_by_tex("a", RED))
 
         node2 = DrawBorderThenFill(Circle(RADIUS, color=CIRCLE_COLOR).move_to([LAYER_1_X,0,0]))
-        text2 = Create(Text("a2", font_size=20, color=RED).move_to([LAYER_1_X,0,0]))
+        text2 = Create(MathTex(r"a_{2}", font_size=35, color=RED).move_to([LAYER_1_X,0,0]).set_color_by_tex("a", RED))
 
         node3 = DrawBorderThenFill(Circle(RADIUS, color=CIRCLE_COLOR).move_to([LAYER_1_X,-2,0]))
-        text3 = Create(Text("a3", font_size=20, color=RED).move_to([LAYER_1_X,-2,0]))
+        text3 = Create(MathTex(r"a_{3}", font_size=35, color=RED).move_to([LAYER_1_X,-2,0]).set_color_by_tex("a", RED))
 
         # Output node
         node4 = DrawBorderThenFill(Circle(RADIUS, color=CIRCLE_COLOR).move_to([LAYER_2_X,-0,0]))
-        text4 = Create(Text("n", font_size=20, color=GREEN).move_to([LAYER_2_X,0,0]))
+        text4 = Create(MathTex("n", font_size=35, color=GREEN).move_to([LAYER_2_X,0,0]).set_color_by_tex("n", GREEN))
 
         fist_layer_positions = [2, 0, -2]
         second_layer_positions = [0]
@@ -145,7 +148,14 @@ class Formula(Scene):
         self.play(*[node1,node2,node3, node4, text1, text2, text3, text4])
         self.play(*Lines)
 
-        form = Create(Text("n = sigmoid(a1*w1 + a2*w2 + a3*w3+ +b)", t2c={'a':RED, 'w':BLUE, 'n':GREEN}, font_size=25).move_to([3,2,0]), run_time=3)
+        form = MathTex(r"n = sigmoid(a_{1}*w_{1} + a_{2}*w_{2} + a_{3}*w_{3} ... + b)",  substrings_to_isolate=["a","w","n"], font_size=35).move_to([3,0,0])
+        form.set_color_by_tex("a", RED)
+        form.set_color_by_tex("w", BLUE)
+        form.set_color_by_tex("n", GREEN)
 
-        self.play(form)
-        self.wait(5)
+        self.play(Create(form, run_time=3))
+        self.wait(10)
+
+class Entrenamiento(Scene):
+    def construct():
+        return None
